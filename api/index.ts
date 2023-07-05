a044b70080e882e51db9852be1a29c26d14a6b5f
Etienne Moureton <accounts@etiennemoureton.fr>
2023-07-05 12:16:09 +0200

    remove appwrite & added steps route

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import UserController from './routes/UserController';
+import StepsController from './routes/StepsController';
 
 dotenv.config();
 
@@ -8,10 +9,11 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+    res.send('API is running 🚀');
 });
 
 app.use('/user', UserController);
+app.use('/steps', StepsController)
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
