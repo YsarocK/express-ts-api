0bfd7d59beda3af4ec0183ba4a6c1d4fe6a213cf
Etienne Moureton <accounts@etiennemoureton.fr>
2023-07-07 11:08:19 +0200

    clean files

--- api/dist/routes/UserController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const UserController = (0, express_1.Router)();
-UserController.get('/link', function (req, res) {
-    res.send("Envoi d'un magic link lol");
-});
-exports.default = UserController;
