ef5f98dbe0f25f808976b8122b6b155a048d529e
NeoxAzrot <sami.lafrance@akwai.co>
2023-07-07 10:53:55 +0200

    fix(*): tracked files

--- api/dist/routes/StepsController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const StepsController = (0, express_1.Router)();
-StepsController.get('/', function (req, res) {
-    res.send("Vérification des étapes");
-});
-exports.default = StepsController;
