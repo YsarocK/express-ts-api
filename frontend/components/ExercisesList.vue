613b8495570b24d928de800dba691384efc67ace
Etienne Moureton <accounts@etiennemoureton.fr>
2023-07-08 13:54:02 +0200

    reworked front components + added logic

--- frontend/components/ExercisesList.vue
+++ frontend/components/ExercisesList.vue
@@ -1,59 +1,60 @@
 <template>
-  <div class="flex flex-col gap-2 w-full max-w-2xl">
+  <div class="flex flex-col gap-4 w-full">
     <div
       v-if="exercises.tests && typeof exercises.tests !== 'string'"
-      class="flex items-center gap-4 p-4 w-full rounded-md bg-slate-50"
+      class="flex items-start gap-4 p-4 w-full rounded-md bg-slate-50"
       v-for="exercise in exercises.tests"
     >
       <div
         v-if="exercise.passed"
-        class="bg-green-100 p-2 w-10 h-10 rounded-md flex"
+        class="bg-green-100 p-2 w-10 h-10 min-w-10 min-h-10 rounded-md flex"
       >
         <nuxt-icon
           name="check"
           class="text-green-400 text-md w-full h-full m-auto"
         />
       </div>
-      <div v-else class="bg-red-100 p-2 w-10 h-10 rounded-md flex">
+      <div v-else class="bg-red-100 p-2 w-10 h-10 min-w-10 min-h-10 rounded-md flex">
         <nuxt-icon
           name="close"
           class="text-red-400 text-md w-full h-full m-auto"
         />
       </div>
-      <div class="flex flex-col gap-1">
-        <p class="text-xl">{{ exercise.title }}</p>
-        <p class="text-sm opacity-80">{{ exercise.description }}</p>
+      <div class="exercises-list__card__content w-full grid gap-3">
+        <p class="text-md leading-none exercises-list__card__content__title">{{ exercise.title }}</p>
+        <p class="text-sm leading-none exercises-list__card__content__points text-slate-400">{{ exercise.points }} point{{ exercise.points > 1 ? 's' : '' }}</p>
+        <p class="text-sm opacity-80 leading-normal exercises-list__card__content__description text-slate-600">{{ exercise.description }}</p>
       </div>
     </div>
     <div v-else>
       <p>Cannot fetch</p>
     </div>
-    <button class="btn mt-5" @click="() => refresh()">Refresh</button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useSessionStore } from '../store/session';
-import { storeToRefs } from 'pinia';
-
-const { user } = storeToRefs(useSessionStore());
-
-const {
-  data: exercises,
-  error,
-  refresh,
-} = await useAsyncData('exercises', async () => {
-  const { apiEndpoint } = useRuntimeConfig().public;
-  const res = await fetch(`${apiEndpoint}/exercises/verify`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      host: user.value.ssh.host,
-      username: user.value.ssh.username,
-    }),
-  });
-  return await res.json();
-});
+const { exercises } = defineProps<{ exercises: any }>()
 </script>
+
+<style lang="scss">
+.exercises-list {
+  &__card {
+    &__content {
+      grid-template-areas: "title points" "description description";
+      grid-template-columns: 1fr auto;
+
+      &__title {
+        grid-area: title;
+      }
+
+      &__points {
+        grid-area: points;
+      }
+
+      &__description {
+        grid-area: description;
+      }
+    }
+  }
+}
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,43 @@
 <template>
-  <div class="w-screen h-screen flex justify-center items-center">
-    <exercises-list />
+  <div class="w-screen h-screen flex flex-col justify-center items-center">
+    <div class="w-full max-w-2xl flex flex-col gap-10">
+      <div>
+        <p class="text-md text-slate-500">Score : <span>{{ exercises.score }} / 20</span></p>
+      </div>
+      <exercises-list :exercises="exercises" />
+      <button class="btn">Refresh</button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { useSessionStore } from '../store/session';
+import { storeToRefs } from 'pinia';
+
+const { user } = storeToRefs(useSessionStore());
+
+const {
+  data: exercises,
+  error,
+} = await useAsyncData('exercises', async () => {
+  const { apiEndpoint } = useRuntimeConfig().public;
+  const res = await fetch(`${apiEndpoint}/exercises/verify`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      host: user.value.ssh.host,
+      username: user.value.ssh.username,
+    }),
+  });
+
+  const json = await res.json()
+
+  if (!json.success) {
+    throw new Error(json.error);
+  }
+
+  return json.data;
+});
+</script>
