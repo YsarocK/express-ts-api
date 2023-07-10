4317493e68a97b8c010ccd60bcc986dce3b99d5d
NeoxAzrot <sami.lafrance@akwai.co>
2023-07-10 14:32:30 +0200

    fix(*): fix docker

--- api/server.ts
+++ api/server.ts
@@ -8,7 +8,7 @@ import './databases/databaseCreate';
 const app = express();
 
 dotenv.config();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const whitelist = process.env.CORS?.split(',') || [];
 const corsOptions: CorsOptions = {
