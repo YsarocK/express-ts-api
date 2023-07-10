f39e5037e9ffda3f87f26ff5a608e67aec57c8e6
NeoxAzrot <sami.lafrance@akwai.co>
2023-07-10 16:08:33 +0200

    fix(*): fix

--- api/models/admin.ts
+++ api/models/admin.ts
@@ -2,25 +2,19 @@ import { DataTypes } from 'sequelize';
 import { sequelize } from 'databases';
 
 export const Admin = sequelize.define('Admin', {
-    //.
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-    },
-    user_id: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 });
 
-Admin.sync()
+Admin.sync();
