25f07a9e5b2070c30877582e26d93b20959e4f7a
PaulBlackMotion <71767507+paulcotogno@users.noreply.github.com>
2023-07-11 12:53:05 +0200

    add types jwt tokens

--- api/middlewares/auth.middleware.ts
+++ api/middlewares/auth.middleware.ts
@@ -7,11 +7,33 @@ export const Auth = async (req: Request, res: Response, next: NextFunction) => {
 
     const userDoc = await JWToken.verifyToken(authToken);
 
-    if(!userDoc.sub) {
+    if(!userDoc.data || !userDoc.data.userId) {
       new Error('Bad Token delivered');
     }
 
-    res.locals.userId = userDoc.sub;
+    res.locals.userId = userDoc.data.userId;
+
+    next();
+  } catch (err) {
+    next(err);
+  }
+}
+
+export const AuthAdmin = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const authToken = req.cookies.token;
+
+    const userDoc = await JWToken.verifyToken(authToken);
+
+    if(!userDoc.data) {
+      new Error('Bad Token delivered');
+    }
+
+    if(!userDoc.data.isAdmin) {
+      new Error('No admin rights');
+    }
+
+    res.locals.userId = userDoc.data.userId;
 
     next();
   } catch (err) {
