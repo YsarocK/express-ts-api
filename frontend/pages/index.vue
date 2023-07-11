9f717902bee56eda6a7834764f5f3c4a3fb82f39
Etienne Moureton <accounts@etiennemoureton.fr>
2023-07-11 13:07:59 +0200

    deleted basi route

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,25 +1,3 @@
 <template>
-  <div class="w-full max-w-2xl flex flex-col gap-10">
-    <informations />
-    <div>
-      <p class="text-md text-slate-500">
-        Score : <span>{{ exercises?.score }} / 20</span>
-      </p>
-    </div>
-    <exercises-list :exercises="exercises" />
-    <button class="btn">Tester</button>
-  </div>
+  <h1>YO LES BOYYYYYYYYYYZ</h1>
 </template>
-
-<script setup lang="ts">
-import { ApiResponsesTypes } from '../types';
-import Informations from "../components/Informations.vue";
-
-const { $api } = useNuxtApp();
-
-const exercises: Ref<undefined | ApiResponsesTypes.Verify> = ref(undefined);
-
-onMounted(async () => {
-  exercises.value = await $api.verifyExercises();
-});
-</script>
