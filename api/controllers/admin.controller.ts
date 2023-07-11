f5ac7bae1d2fad886dc83969b9895967e0b6f5fc
Nexk0 <pierrekeller75@gmail.com>
2023-07-11 17:22:54 +0200

    Added new service on controller

--- api/controllers/admin.controller.ts
+++ api/controllers/admin.controller.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express';
 import {SessionService} from "../services/session.service";
+import {UserSessionService} from "../services/usersession.service";
 
-export const createSession = async (req: Request<{}, {}, {name: string, isActive:boolean}, { }>, res: Response) => {
-    const {name, isActive}: {name : string, isActive : boolean} = req.body;
+export const createSession = async (req: Request<{}, {}, {name: string}, { }>, res: Response) => {
+    const {name}: {name : string} = req.body;
 
     console.log(name)
 
-    const session = await SessionService.generateSession(name, isActive)
+    const session = await SessionService.generateSession(name)
 
     return res.status(200).json({
         session: session,
@@ -29,5 +30,14 @@ export const getAllSessions = async (req: Request, res: Response) => {
 
     return res.status(200).json({
         sessions: sessions,
+    })
+}
+
+export const getAllUsersSessionById = async (req: Request<{session_id: string}, {}, {}, { }>, res: Response) => {
+    const {session_id}: {session_id : string} = req.params;
+    const users_sessions = await UserSessionService.getAllUsersSessionById(session_id)
+
+    return res.status(200).json({
+        users_sessions: users_sessions,
     })
 }
