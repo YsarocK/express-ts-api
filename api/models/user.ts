4a68f08ce846059e3e74df7218dcab9f0448f0b1
Nexk0 <pierrekeller75@gmail.com>
2023-07-11 11:38:24 +0200

    Rechanged model

--- api/models/user.ts
+++ api/models/user.ts
@@ -1,53 +1,26 @@
-import { DataTypes, Model, Optional } from 'sequelize';
+import { DataTypes } from 'sequelize';
 import { sequelize } from 'databases';
 
-interface UserAttributes {
-    id: string;
-    email: string;
-    firstname: string;
-    lastname: string;
-}
-
-interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
-
-class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
-    public id!: string;
-    public email!: string;
-    public firstname!: string;
-    public lastname!: string;
-
-    public readonly createdAt!: Date;
-    public readonly updatedAt!: Date;
-}
-
-User.init(
-    {
-        id: {
-            type: DataTypes.UUID,
-            defaultValue: DataTypes.UUIDV4,
-            allowNull: false,
-            primaryKey: true,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        firstname: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        lastname: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+export const User = sequelize.define('User', {
+    //.
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
     },
-    {
-        sequelize,
-        modelName: 'User',
-    }
-);
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    firstname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
 
-User.sync();
-
-export default User;
-//
+User.sync()
