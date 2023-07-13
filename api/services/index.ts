91ed50428b796a50961757ce01e4e5efcf1706ce
NeoxAzrot <sami.lafrance@akwai.co>
2023-07-13 11:00:56 +0200

    fix(*): rename

--- api/services/index.ts
+++ api/services/index.ts
@@ -2,4 +2,4 @@ export * from './admin.service';
 export * from './exercise.service';
 export * from './session.service';
 export * from './user.service';
-export * from './userSession.service';
+export * from './userSession1.service';
