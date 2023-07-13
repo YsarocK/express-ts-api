4b9c4291e8e10478b4d42848022276488dcd4ca3
Etienne Moureton <accounts@etiennemoureton.fr>
2023-07-13 12:04:08 +0200

    trying to change info

--- api/utils/nodemailer.ts
+++ api/utils/nodemailer.ts
@@ -5,11 +5,12 @@ dotenv.config();
 
 export const sendMailNodemailer = async (message: string, email: string) => {
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.NODERMAILER_EMAIL as string,
-            pass: process.env.NODEMAILER_PASSWORD as string,
-        }
+      host: 'mail.infomaniak.com',
+      port:465,
+      auth: {
+          user: process.env.NODERMAILER_EMAIL as string,
+          pass: process.env.NODEMAILER_PASSWORD as string,
+      }
     });
 
 
@@ -20,15 +21,15 @@ export const sendMailNodemailer = async (message: string, email: string) => {
     text: message,
   };
 
-    await new Promise((resolve, reject) => {
-        transporter.sendMail(mailOptions, (err, info) => {
-            if (err) {
-                console.error(err);
-                reject(err);
-            } else {
-                resolve(info);
-                console.log(info)
-            }
-        });
-    });
+  return await new Promise((resolve, reject) => {
+      return transporter.sendMail(mailOptions, (err, info) => {
+          if (err) {
+              console.error(err);
+              reject(err);
+          } else {
+              console.log(info)
+              resolve(info);
+          }
+      });
+  });
 };
